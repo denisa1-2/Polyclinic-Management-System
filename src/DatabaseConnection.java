1c1bef19f01b6b39be99b5f4202b71b39511bfc0
denisaraileanu <denisaraileanu99@gmail.com>
2026-03-26 16:47:33 +0200

    Update DatabaseConnection.java

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/policlinica";
     private static final String USER = "root";
-    private static final String PASSWORD = "Denisa12."; // Schimbați cu parola MySQL
+    private static final String PASSWORD = "root"; // Schimbați cu parola MySQL
 
     public static Connection getConnection() throws SQLException {
 
